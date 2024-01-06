e5075c3cc6e04aafb75c773337831d3fb0f8c98c
345257 <sbrrllss500@gmail.com>
2024-01-06 15:24:06 +0900

    8-3-21

--- src/components/CompMovieList.vue
+++ src/components/CompMovieList.vue
@@ -1,7 +1,13 @@
 <template>
   <div class="container">
-    <div class="inner">
-      <CompMovieItem v-for="movie in movies" :key="movie.imdbID" :movie="movie"></CompMovieItem>
+    <div :class="{ 'no-result': !movies.length }" class="inner">
+      <div v-if="loading" class="spinner-border text-primary"></div>
+      <div v-if="message" class="message">
+        {{ message }}
+      </div>
+      <div v-else class="movies">
+        <CompMovieItem v-for="movie in movies" :key="movie.imdbID" :movie="movie"></CompMovieItem>
+      </div>
     </div>
   </div>
 </template>
@@ -16,7 +22,38 @@
     computed: {
       movies() {
         return this.$store.state.movie.movies
+      }, 
+      message() {
+        return this.$store.state.movie.message
+      }, 
+      loading() {
+        return this.$store.state.movie.loading
       }
     }
   }
 </script>
+
+<style lang="scss" scoped>
+  @import "../scss/main.scss";
+  .container {
+    margin-top: 30px;
+    .inner {
+      background-color: $gray-200;
+      padding: 10px 0;
+      border-radius: 4px;
+      text-align: center;
+      &.no-result {
+        padding: 70px 0;
+      }
+    }
+    .message {
+      color: $gray-400;
+      font-size: 20px;
+    }
+    .movies {
+      display: flex;
+      flex-wrap: wrap;
+      justify-content: center;
+    }
+  }
+</style>
