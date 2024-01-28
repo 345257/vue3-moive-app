bf79ce013c7ac4da83de541bb5611ce72b546aaf
345257 <sbrrllss500@gmail.com>
2024-01-28 23:36:59 +0900

    add e2e tests

--- functions/movie.js
+++ functions/movie.js
@@ -2,6 +2,7 @@ const axios = require('axios')
 const { OMDB_API_KEY } = process.env
 
 exports.handler = async function(event) {
+  console.log(OMDB_API_KEY)
   const payload = JSON.parse(event.body)
   const { title, type, year, page, id } = payload
   const url = id ? `https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&i=${id}` : `https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type=${type}&y=${year}&page=${page}`
